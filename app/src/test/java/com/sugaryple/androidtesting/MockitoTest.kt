352a8f9634144aae0ae6af01aaca51e7d363512c
Simon.S <seaud5@gmail.com>
2018-09-30 00:22:39 +0900

    mockを使ってLinkedListを検証して見ました

--- app/src/test/java/com/sugaryple/androidtesting/MockitoTest.kt
+++ app/src/test/java/com/sugaryple/androidtesting/MockitoTest.kt
@@ -1,10 +1,10 @@
 package com.sugaryple.androidtesting
 
+import com.nhaarman.mockitokotlin2.mock
 import org.junit.Test
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
+import org.mockito.Mockito.*
 import java.util.*
 
 class MockitoTest {
@@ -22,5 +22,20 @@ class MockitoTest {
 
         //[999]は返還値が指定しないのでnull
         assertNull(mockedList[999])
+    }
+
+    @Test
+    fun mockを使ってLinkedListを検証して見ました() {
+        //Mockオブジェクト生成
+        //mockを使うkotlinではgenericsするのが難しくてライブラリを使いました。(http://bit.ly/2xYXqkN)
+        val mockedList : LinkedList<String> = mock()
+
+        //メソッドを呼び出す(ただ、返還値や動作を設定しなかったので何の事も起きてない)
+        mockedList.add("one")
+        mockedList.clear()
+
+        //addとclearメソッドが１回づつ呼び出したことを検証
+        verify(mockedList, times(1)).add("one")
+        verify(mockedList, times(1)).clear()
     }
 }
